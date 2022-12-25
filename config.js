dd02364fbe3fcc6f43ceb3221dcaaef2b60890fc
Alien Tapia Salvador <alientapia33@gmail.com>
2022-12-24 23:09:56 -0500

    base

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
   port: process.env.PORT,
-  mongo_uri: process.env.URI || 'mongodb://localhost/linkdb',
-  baseUrl: process.env.BASE_URL || `http://localhost:4000`,
+  mongo_uri: process.env.URI,
+  baseUrl: process.env.BASE_URL,
 };
